8bc39b1d67b9529cac0bef65631792d50da55221
Garima Goel <96510150+Garimaagoel@users.noreply.github.com>
2022-12-28 22:07:15 +0530

    Add files via upload

---
+++ linearsearch.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+
+int linearsearch(int arr[],int n, int key)
+{
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==key){
+            return i;
+        }
+
+        
+    }
+    return -1;
+}
+int main()
+{
+    int n;
+    cout<<"enter number of elements : ";
+    cin>>n;
+    int arr[50];
+    cout<<"enter array elements : ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int key;
+    cout<<"enter element to be found : "<<endl;
+    cin>>key;
+
+cout<<linearsearch(arr,n,key)<<endl;
+}
